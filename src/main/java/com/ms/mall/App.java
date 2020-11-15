a69994b861ab1836a84f2d3731197b4dddcb5383
yehaijin <869754518@qq.com>
2020-11-15 23:49:53 +0800

    分支添加

--- src/main/java/com/ms/mall/App.java
+++ src/main/java/com/ms/mall/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "创建分支" );
         System.out.println( "分支添加" );
         System.out.println( "主干添加" );
+        System.out.println( "分支新增" );
     }
 }
